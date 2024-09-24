6bf8183c7f24438a928cd35083364ed253767b7d
sunnymendapara7 <sunnymendapara09@gmail.com>
2024-09-24 14:51:40 +0530

    Add project files after pulling .gitignore and README

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing  import List
+
+
+def get_requirements()->List[str]:
+
+    requirements_list = []
+
+    return requirements_list
+
+
+
+
+
+
+
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='sunny',
+    author_email='sunnymendapara09@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+
+)
